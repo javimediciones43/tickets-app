5771633e2caddecd617221ff131ca9cbe115ef54
javimediciones <javiero_212@hotmail.com>
2025-05-21 16:35:16 -0300

    configura el modelo Provincia.php

--- app/Models/Provincia.php
+++ app/Models/Provincia.php
@@ -3,8 +3,35 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Provincia extends Model
 {
-    //
+    use SoftDeletes;
+
+    /**
+     * Indicates if the model's ID is auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = true;
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'provincias';
+
+    protected $fillable = [
+        'name',
+        'codigo'
+    ];
 }
