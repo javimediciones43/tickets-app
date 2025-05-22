7f8095baaaa287bc29e7c71427ef708152248418
javimediciones <javiero_212@hotmail.com>
2025-05-21 23:15:02 -0300

    Agrega columna provincia_id a la tabla usuario

--- database/migrations/0001_01_00_000000_create_provincias_table.php
+++ database/migrations/0001_01_00_000000_create_provincias_table.php
@@ -13,7 +13,10 @@ return new class extends Migration
     {
         Schema::create('provincias', function (Blueprint $table) {
             $table->id();
+            $table->string('nombre');
+            $table->integer('codigo');
             $table->timestamps();
+            $table->softDeletes();  
         });
     }
 
